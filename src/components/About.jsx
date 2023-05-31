99ab0cb8e40200c5771c94c7ee2f9120e7d03310
Luis <lrangarita7@gmail.com>
2023-05-31 17:47:33 -0300

    About mejora visual

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,7 @@ const About = () => {
   return (
     <section id="About" className="flex flex-col md:w-1/2 mx-auto mb-36">
       <div className="flex flex-row w-1/2 mb-10 ">
-        <h2 className="text-warning mt-auto mr-2">01.</h2>
-        <h2 className="text-2xl text-slate-200 font-bold border-b border-warning">
-          About me
-        </h2>
+        <h2 className="text-2xl text-slate-200 font-bold">About me</h2>
       </div>
       <div className="md:flex md:flex-row">
         <div className="md:w-1/2 md:order-1 mb-4 md:text-lg">
@@ -22,7 +19,7 @@ const About = () => {
           </p>
         </div>
         <div className="md:w-1/2 md:order-2">
-          <div className="avatar relative flex flex-row rounded-full w-32 md:w-60 bg-base-100 mx-auto">
+          <div className="avatar relative flex flex-row rounded-full w-32 md:w-60 mx-auto">
             <div className="w-32 md:w-60 rounded ring ring-gray-400 ring-offset-base-100 ring-offset-2">
               <img
                 src={avatar}
