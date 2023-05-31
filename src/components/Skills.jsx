1c383fd226b6bfd6f51c4efd9c797a32e3ad27bf
Luis <lrangarita7@gmail.com>
2023-05-31 17:47:23 -0300

    Skills update visual

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,30 +4,55 @@ import css3 from "../assets/img/css3.svg";
 import javascript from "../assets/img/javascript.svg";
 import react from "../assets/img/react.svg";
 import tailwind from "../assets/img/tailwind.svg";
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiPostman,
+  SiGithub,
+  SiTailwindcss,
+  SiBootstrap,
+} from "react-icons/si";
 
 const Skills = () => {
   return (
     <div className="flex flex-col mb-20 md:w-1/2 mx-auto">
       <div className="flex flex-row mb-10">
-        <h2 className="ml-2 text-2xl text-slate-200 font-bold border-b border-warning">
-          Tech stack
-        </h2>
+        <h2 className="ml-2 text-2xl text-slate-200 font-bold">Tech stack</h2>
       </div>
-      <ul className="flex flex-wrap gap-8 justify-center">
-        <li>
-          <img src={html5} alt="html" width={34} />
+      <ul className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiHtml5 size="1.3rem" />
+          <p className="text-white text-base ml-2"> Html</p>
         </li>
-        <li>
-          <img src={css3} alt="html" width={34} />
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiCss3 size="1.3rem" />
+          <p className="text-white text-base ml-2"> Css</p>
         </li>
-        <li>
-          <img src={javascript} alt="html" width={34} />
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiJavascript size="1.3rem" />
+          <p className="text-white text-base ml-2"> Javascript</p>
         </li>
-        <li>
-          <img src={react} alt="html" width={34} />
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiReact size="1.3rem" />
+          <p className="text-white text-base ml-2"> React</p>
         </li>
-        <li>
-          <img src={tailwind} alt="html" width={34} />
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiTailwindcss size="1.3rem" />
+          <p className="text-white text-base ml-2"> TailwindCss</p>
+        </li>
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiBootstrap size="1.3rem" />
+          <p className="text-white text-base ml-2"> Bootstrap</p>
+        </li>
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiGithub size="1.3rem" />
+          <p className="text-white text-base ml-2"> Github</p>
+        </li>
+        <li className="flex flex-row border-2 border-white rounded-2xl p-2">
+          <SiPostman size="1.3rem" />
+          <p className="text-white text-base ml-2"> Postman</p>
         </li>
       </ul>
     </div>
