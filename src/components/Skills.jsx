673deadb75e2fdaefe12f5b5a61f5ac03fcbe3c8
Louisit0 <lrangarita7@gmail.com>
2023-06-01 16:10:48 -0300

    fix icons alignment

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,37 +21,37 @@ const Skills = () => {
       <div className="flex flex-row mb-10">
         <h2 className="ml-2 text-2xl text-slate-200 font-bold">Tech stack</h2>
       </div>
-      <ul className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <ul className="grid grid-cols-2 xl:grid-cols-4 gap-4">
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiHtml5 size="1.3rem" />
+          <SiHtml5 size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> Html</p>
         </li>
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiCss3 size="1.3rem" />
+          <SiCss3 size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> Css</p>
         </li>
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiJavascript size="1.3rem" />
+          <SiJavascript size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> Javascript</p>
         </li>
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiReact size="1.3rem" />
+          <SiReact size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> React</p>
         </li>
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiTailwindcss size="1.3rem" />
+          <SiTailwindcss size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> TailwindCss</p>
         </li>
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiBootstrap size="1.3rem" />
+          <SiBootstrap size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> Bootstrap</p>
         </li>
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiGithub size="1.3rem" />
+          <SiGithub size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> Github</p>
         </li>
         <li className="flex flex-row border-2 border-white rounded-2xl p-2">
-          <SiPostman size="1.3rem" />
+          <SiPostman size="1.3rem" className="self-center" />
           <p className="text-white text-base ml-2"> Postman</p>
         </li>
       </ul>
