1ba4e619a48334504515d9d4bd6bacb0ac91a01a
Louisit0 <lrangarita7@gmail.com>
2023-06-01 16:09:20 -0300

    Changes in homecover, better looking

--- src/components/HomeCover.jsx
+++ src/components/HomeCover.jsx
@@ -12,19 +12,19 @@ const HomeCover = () => {
       <h1 className="text-white font-semibold mb-3 whitespace-pre">
         Hi, my name is
       </h1>
-      <h3 className="text-4xl font-bold text-slate-200 mb-1 md:text-6xl">
+      <h3 className="text-4xl font-bold text-slate-200 mb-1 md:text-5xl">
         Luis Ramirez
       </h3>
-      <h3 className="text-4xl font-bold md:text-6xl">
+      <h3 className="text-4xl font-bold md:text-5xl">
         Frontend React Developer
       </h3>
-      <p className="text-base mt-6 md:text-xl md:w-1/2">
+      <p className="text-base mt-6 md:text-lg w-4/5 md:w-2/3">
         I’m a enthusiast Front-end React Developer jr based in Buenos Aires,
         Argentina. 📍
       </p>
       <div className="flex flex-row my-10">
-        <BsLinkedin size="2rem" className="mr-4 hover:fill-warning" />
-        <BsGithub size="2rem" className="hover:fill-warning" />
+        <BsLinkedin size="1.5rem" className="mr-4 hover:fill-warning" />
+        <BsGithub size="1.5rem" className="hover:fill-warning" />
       </div>
     </section>
   );
