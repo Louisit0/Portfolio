c4df88200a247e663fc5f41b378e80ec20cd123b
Louisit0 <lrangarita7@gmail.com>
2023-06-01 16:10:05 -0300

    eliminate imports, new improve on design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,36 @@
-import React, { useState } from "react";
-import { Outlet, Link } from "react-router-dom";
-import { BsHouseDoorFill, BsCodeSlash, BsPersonFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Nav = ({ togglerDrawer, drawerOpen }) => {
   return (
-    <header
-      className="w-auto md:w-auto py-4 top-0 z-50 lg:w-1/2 lg:px-4 mx-auto"
+    <nav
+      className="flex flex-col lg:flex-row justify-between fixed top-0 w-full py-4 z-50"
       style={{ backgroundColor: "#121212" }}
     >
-      <nav className=" flex flex-col lg:flex-row justify-between">
-        <div className="flex justify-between flex-row">
-          <Link to={"/"} className="text-2xl my-auto ml-4 font-bold md:ml-0">
-            <span className="text-white p-2 mr-1">Portfolio</span>
-          </Link>
-          <label className="swap swap-rotate mr-4 lg:hidden">
-            {/* this hidden checkbox controls the state  */}
-            <input
-              id="my-drawer-4"
-              onClick={togglerDrawer}
-              type="checkbox"
-              defaultChecked={drawerOpen}
-            />
+      <div className="flex flex-row justify-between lg:justify-evenly w-full lg:mx-auto">
+        <Link to={"/"} className="text-2xl my-auto mx-4 font-bold">
+          <span className="text-white">Portfolio</span>
+        </Link>
+        <label className="swap swap-rotate mr-4 lg:hidden">
+          {/* this hidden checkbox controls the state  */}
+          <input
+            id="my-drawer-4"
+            onClick={togglerDrawer}
+            type="checkbox"
+            defaultChecked={drawerOpen}
+          />
 
-            {/* hamburger icon */}
-            <svg
-              className="fill-current text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 512 512"
-            >
-              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-            </svg>
-          </label>
-        </div>
-        <div className="hidden lg:flex flex-row items-center justify-between w-3/5">
+          {/* hamburger icon */}
+          <svg
+            className="fill-current text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+          </svg>
+        </label>
+        <div className="hidden lg:flex flex-row items-center justify-between w-2/5">
           <a
             onClick={togglerDrawer}
             className=" select-none cursor-pointer hover:text-warning active:text-warning transition-all"
@@ -65,13 +61,13 @@ const Nav = ({ togglerDrawer, drawerOpen }) => {
           </a>
           <a
             href="#"
-            className="btn btn-outline rounded-2xl w-1/2 mt-10 md:my-auto md:w-auto hover:bg-white"
+            className="px-3 py-1 border-2 border-white text-white rounded-2xl hover:text-black hover:bg-white"
           >
-            Resume
+            Curriculum
           </a>
         </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
   );
 };
 
