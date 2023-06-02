0ef98fac7b4e6588bff407f074f731e8d2230eb5
Louisit0 <lrangarita7@gmail.com>
2023-06-02 15:55:40 -0300

    fix responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,19 +9,19 @@ const Projects = () => {
       <div className="flex flex-row mb-10">
         <h2 className="text-2xl text-slate-200 font-bold">Projects</h2>
       </div>
-      <ul className="flex flex-wrap md:justify-evenly gap-y-6 md:gap-y-14">
-        <li>
-          <figure>
+      <ul className="grid grid-cols-1 md:justify-evenly gap-y-6 md:gap-y-20">
+        <li className="md:flex md:flex-row md:justify-between">
+          <figure className="md:w-1/2">
             <img
               src={imgg}
               alt="prj"
               className="object-cover h-full border-2 border-white rounded-3xl"
             />
           </figure>
-          <div className="flex flex-col gap-6 py-6 px-2">
+          <span className="mx-2"></span>
+          <div className="flex flex-col gap-6 md:w-1/2">
             <h2 className="card-title font-bold">Dashboard</h2>
-
-            <p className="md:w-4/5">
+            <p>
               A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm,
               and more. Available on app store.
             </p>
@@ -42,18 +42,11 @@ const Projects = () => {
             </div>
           </div>
         </li>
-        <li>
-          <figure>
-            <img
-              src={imgg}
-              alt="prj"
-              className="object-cover h-full rounded-3xl"
-            />
-          </figure>
-          <div className="flex flex-col gap-6 py-6 px-2">
+        <li className="flex flex-col-reverse md:flex-row md:justify-between">
+          <div className="flex flex-col gap-6 md:w-1/2">
             <h2 className="card-title font-bold">Dashboard</h2>
 
-            <p className="md:w-4/5">
+            <p>
               A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm,
               and more. Available on app store.
             </p>
@@ -73,71 +66,82 @@ const Projects = () => {
               />
             </div>
           </div>
-        </li>
-        <li>
-          <figure>
+          <span className="mx-2"></span>
+          <figure className="md:w-1/2">
             <img
               src={imgg}
               alt="prj"
               className="object-cover h-full border-2 border-white rounded-3xl"
             />
           </figure>
-          <div className="flex flex-col gap-6 py-6 px-2">
-            <h2 className="card-title font-bold">Dashboard</h2>
-
-            <p className="md:w-4/5">
-              A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm,
-              and more. Available on app store.
-            </p>
-            <ul className="flex flex-row  font-extralight text-sm">
-              <li>React.js</li>
-              <li className="mx-4">Tailwind CSS</li>
-              <li>Firebase</li>
-            </ul>
-            <div className="flex flex-row w-full">
-              <BsGithub
-                className="mr-4 my-auto hover:fill-warning"
-                size="1.5rem"
-              />
-              <BsBoxArrowUpRight
-                className="my-auto hover:fill-warning"
-                size="1.2rem"
-              />
-            </div>
-          </div>
         </li>
-        <li>
-          <figure>
-            <img
-              src={imgg}
-              alt="prj"
-              className="object-cover h-full border-2 border-white rounded-3xl"
-            />
-          </figure>
-          <div className="flex flex-col gap-6 py-6 px-2">
-            <h2 className="card-title font-bold">Dashboard</h2>
+        {/* <div className="md:flex md:flex-row">
+          <li>
+            <figure>
+              <img
+                src={imgg}
+                alt="prj"
+                className="object-cover h-full rounded-3xl"
+              />
+            </figure>
+            <div className="flex flex-col gap-6 py-6 px-2">
+              <h2 className="card-title font-bold">Dashboard</h2>
 
-            <p className="md:w-4/5">
-              A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm,
-              and more. Available on app store.
-            </p>
-            <ul className="flex flex-row  font-extralight text-sm">
-              <li>React.js</li>
-              <li className="mx-4">Tailwind CSS</li>
-              <li>Firebase</li>
-            </ul>
-            <div className="flex flex-row w-full">
-              <BsGithub
-                className="mr-4 my-auto hover:fill-warning"
-                size="1.5rem"
-              />
-              <BsBoxArrowUpRight
-                className="my-auto hover:fill-warning"
-                size="1.2rem"
-              />
+              <p>
+                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
+                iTerm, and more. Available on app store.
+              </p>
+              <ul className="flex flex-row  font-extralight text-sm">
+                <li>React.js</li>
+                <li className="mx-4">Tailwind CSS</li>
+                <li>Firebase</li>
+              </ul>
+              <div className="flex flex-row w-full">
+                <BsGithub
+                  className="mr-4 my-auto hover:fill-warning"
+                  size="1.5rem"
+                />
+                <BsBoxArrowUpRight
+                  className="my-auto hover:fill-warning"
+                  size="1.2rem"
+                />
+              </div>
             </div>
-          </div>
-        </li>
+          </li>
+          <span className="m-0 md:mx-2"></span>
+          <li>
+            <figure>
+              <img
+                src={imgg}
+                alt="prj"
+                className="object-cover h-full border-2 border-white rounded-3xl"
+              />
+            </figure>
+            <div className="flex flex-col gap-6 py-6 px-2">
+              <h2 className="card-title font-bold">Dashboard</h2>
+
+              <p>
+                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
+                iTerm, and more. Available on app store.
+              </p>
+              <ul className="flex flex-row  font-extralight text-sm">
+                <li>React.js</li>
+                <li className="mx-4">Tailwind CSS</li>
+                <li>Firebase</li>
+              </ul>
+              <div className="flex flex-row w-full">
+                <BsGithub
+                  className="mr-4 my-auto hover:fill-warning"
+                  size="1.5rem"
+                />
+                <BsBoxArrowUpRight
+                  className="my-auto hover:fill-warning"
+                  size="1.2rem"
+                />
+              </div>
+            </div>
+          </li>
+        </div> */}
       </ul>
       <button className="mx-auto w-1/2 normal-case btn bg-white text-black hover:bg-white mt-20 mb-36 ">
         Show More
