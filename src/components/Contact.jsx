4a4a103af5e49608f7a1b0b8d5b1331cf389f8d8
Luis <lrangarita7@gmail.com>
2023-06-05 01:32:56 -0300

    font size, and color text changes.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,48 +12,30 @@ const Contact = () => {
   return (
     <section id="Contact" className="flex flex-col pb-5">
       <div className="flex flex-row">
-        <h2 className="text-2xl text-slate-200 font-bold">Contact</h2>
+        <h2 className="text-2xl text-white font-bold">Contact</h2>
       </div>
-      <div className="md:mx-0 mb-16">
-        <h3 className="text-xl font-bold text-slate-200 md:text-xl my-4">
+      <div className="md:mx-0 mb-16 text-slate-200">
+        <h3 className="text-xl font-bold md:text-xl my-4">
           Don't be shy! Get in touch! 👇
         </h3>
-        <div className="grid grid-cols-2 gap-3 mt-5 ">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-3 mt-5 ">
           <div className="flex flex-row justify-center py-2 border-2 border-white rounded-2xl">
-            <BsFillEnvelopeFill size="1.2rem" />
+            <BsFillEnvelopeFill size="1.2rem" className="self-center" />
             <h3 className="ml-2">Mail</h3>
           </div>
           <div className="flex flex-row justify-center py-2 border-2 border-white rounded-2xl">
-            <BsLinkedin size="1.2rem" />
+            <BsLinkedin size="1.2rem" className="self-center" />
             <h3 className="ml-2">Linkedin</h3>
           </div>
           <div className="flex flex-row justify-center py-2 border-2 border-white rounded-2xl">
-            <BsGithub size="1.2rem" />
+            <BsGithub size="1.2rem" className="self-center" />
             <h3 className="ml-2">Github</h3>
-          </div>
-          <div className="flex flex-row justify-center py-2 border-2 border-white rounded-2xl">
-            <BsFillPinFill size="1.2rem" />
-            <h3 className="ml-2">Buenos Aires</h3>
           </div>
         </div>
       </div>
-      {/* <div className="mt-10">
-        <ul className="flex flex-wrap gap-16  justify-center">
-          <li>
-            <BsGithub size="1.5rem" className="hover:fill-warning" />
-          </li>
-          <li>
-            <BsLinkedin size="1.5rem" className=" hover:fill-warning" />
-          </li>
-          <li>
-            <BsInstagram size="1.5rem" className=" hover:fill-warning" />
-          </li>
-          <li>
-            <BsTwitter size="1.5rem" className=" hover:fill-warning" />
-          </li>
-        </ul>
-      </div> */}
-      <p className="mt-8 text-center text-sm">Made by Luis Ramirez</p>
+      <p className="mt-8 text-center text-sm text-white">
+        Made by Luis Ramirez 🙌
+      </p>
     </section>
   );
 };
