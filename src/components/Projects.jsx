546351d65c5ad1898a44117aebed7edaf5bcacc2
Luis <lrangarita7@gmail.com>
2023-06-05 01:33:32 -0300

    gap, text color and btn changes.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
       <div className="flex flex-row mb-10">
         <h2 className="text-2xl text-slate-200 font-bold">Projects</h2>
       </div>
-      <ul className="flex flex-wrap gap-y-20">
+      <ul className="flex flex-wrap gap-y-20 md:gap-y-40">
         <li className="md:flex md:flex-row md:justify-between">
           <figure className="md:w-1/2">
             <img
@@ -19,14 +19,14 @@ const Projects = () => {
             />
           </figure>
           <div className="flex flex-col gap-6 md:max-w-sm mt-6 sm:mt-0">
-            <h2 className="card-title font-bold">Dashboard</h2>
-            <p className="my-auto">
+            <h2 className="card-title font-bold text-white">Dashboard</h2>
+            <p className="my-auto text-slate-200">
               Get up and running within minutes. Just install the starter, add
               your content, deploy it, and there you go! This starter works
               seamlessly with hosts like Netlify. Photo by Clément H on
               Unsplash.
             </p>
-            <ul className="flex flex-row font-extralight text-sm mt-auto">
+            <ul className="flex flex-row font-extralight text-sm mt-auto text-slate-200">
               <li>React.js</li>
               <li className="mx-4">Tailwind CSS</li>
               <li>Firebase</li>
@@ -45,19 +45,19 @@ const Projects = () => {
         </li>
         <li className="flex flex-col-reverse md:flex-row md:justify-between">
           <div className="flex flex-col gap-6 md:max-w-sm mt-6 sm:mt-0">
-            <h2 className="card-title font-bold">Dashboard</h2>
-            <p className="my-auto">
+            <h2 className="card-title font-bold text-white">Dashboard</h2>
+            <p className="my-auto text-slate-200">
               Get up and running within minutes. Just install the starter, add
               your content, deploy it, and there you go! This starter works
               seamlessly with hosts like Netlify. Photo by Clément H on
               Unsplash.
             </p>
-            <ul className="flex flex-row  font-extralight text-sm mt-auto">
+            <ul className="flex flex-row  font-extralight text-sm mt-auto text-slate-200">
               <li>React.js</li>
               <li className="mx-4">Tailwind CSS</li>
               <li>Firebase</li>
             </ul>
-            <div className="flex flex-row w-full">
+            <div className="flex flex-row w-full ">
               <BsGithub
                 className="mr-4 my-auto hover:fill-warning"
                 size="1.5rem"
@@ -76,75 +76,8 @@ const Projects = () => {
             />
           </figure>
         </li>
-        {/* <div className="md:flex md:flex-row">
-          <li>
-            <figure>
-              <img
-                src={imgg}
-                alt="prj"
-                className="object-cover h-full rounded-3xl"
-              />
-            </figure>
-            <div className="flex flex-col gap-6 py-6 px-2">
-              <h2 className="card-title font-bold">Dashboard</h2>
-
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on app store.
-              </p>
-              <ul className="flex flex-row  font-extralight text-sm">
-                <li>React.js</li>
-                <li className="mx-4">Tailwind CSS</li>
-                <li>Firebase</li>
-              </ul>
-              <div className="flex flex-row w-full">
-                <BsGithub
-                  className="mr-4 my-auto hover:fill-warning"
-                  size="1.5rem"
-                />
-                <BsBoxArrowUpRight
-                  className="my-auto hover:fill-warning"
-                  size="1.2rem"
-                />
-              </div>
-            </div>
-          </li>
-          <span className="m-0 md:mx-2"></span>
-          <li>
-            <figure>
-              <img
-                src={imgg}
-                alt="prj"
-                className="object-cover h-full border-2 border-white rounded-3xl"
-              />
-            </figure>
-            <div className="flex flex-col gap-6 py-6 px-2">
-              <h2 className="card-title font-bold">Dashboard</h2>
-
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more. Available on app store.
-              </p>
-              <ul className="flex flex-row  font-extralight text-sm">
-                <li>React.js</li>
-                <li className="mx-4">Tailwind CSS</li>
-                <li>Firebase</li>
-              </ul>
-              <div className="flex flex-row w-full">
-                <BsGithub
-                  className="mr-4 my-auto hover:fill-warning"
-                  size="1.5rem"
-                />
-                <BsBoxArrowUpRight
-                  className="my-auto hover:fill-warning"
-                  size="1.2rem"
-                />
-              </div>
-            </div>
-          </li>
-        </div> */}
       </ul>
-      <button className="mx-auto w-1/2 normal-case btn bg-white text-black hover:bg-white mt-20 mb-36 ">
+      <button className="mx-auto w-1/2 md:w-1/3 normal-case btn bg-white text-black hover:bg-white mt-20 mb-36 ">
         Show More
       </button>
     </section>
