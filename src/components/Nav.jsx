9603e68a676f7a9cfa333ad30edd330176e6ebff
Louisit0 <lrangarita7@gmail.com>
2023-07-11 14:06:09 -0300

    se añadio wouter y cambios visuales

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from "wouter";
 
 const Nav = ({ togglerDrawer, drawerOpen }) => {
   return (
-    <nav
-      className="flex flex-col lg:flex-row fixed top-0 w-full py-4 z-50"
-      style={{ backgroundColor: "#121212" }}
-    >
+    <nav className="flex flex-col fixed lg:flex-row w-full py-4">
       <div className="flex flex-row justify-between items-center h-full w-full max-w-4xl lg:mx-auto">
-        <Link to={"/"} className="text-2xl my-auto ml-8 sm:ml-0 font-bold">
-          <span className="text-white text-2xl md:text-3xl">Portfolio</span>
+        <Link
+          to="/"
+          className="my-auto font-bold text-2xl md:text-xl ml-8 lg:ml-0"
+        >
+          @louisit0
         </Link>
         <label className="swap swap-rotate mr-4 lg:hidden">
           {/* this hidden checkbox controls the state  */}
@@ -21,7 +21,7 @@ const Nav = ({ togglerDrawer, drawerOpen }) => {
 
           {/* hamburger icon */}
           <svg
-            className="fill-current text-white mr-2 sm:mr-0"
+            className="fill-current  mr-2 sm:mr-0"
             xmlns="http://www.w3.org/2000/svg"
             width="32"
             height="32"
@@ -31,40 +31,38 @@ const Nav = ({ togglerDrawer, drawerOpen }) => {
           </svg>
         </label>
         <div className="hidden lg:flex flex-row items-center justify-between w-3/5">
-          <a
+          <Link
+            to="/about"
             onClick={togglerDrawer}
-            className=" select-none cursor-pointer hover:text-warning active:text-warning transition-all text-white font-bold"
-            href="#Home"
-          >
-            Home
-          </a>
-          <a
-            onClick={togglerDrawer}
-            className=" select-none cursor-pointer hover:text-warning active:text-warning transition-all text-white font-bold"
+            className=" select-none cursor-pointer transition-all  font-bold"
             href="#About"
           >
             About
-          </a>
-          <a
+          </Link>
+          <Link
+            to="/about"
             onClick={togglerDrawer}
-            className=" select-none cursor-pointer hover:text-warning active:text-warning transition-all text-white font-bold"
-            href="#Projects"
+            className=" select-none cursor-pointer transition-all  font-bold"
+            href="#About"
           >
             Projects
-          </a>
-          <a
+          </Link>
+          <Link
+            to="/about"
             onClick={togglerDrawer}
-            className=" select-none cursor-pointer hover:text-warning active:text-warning transition-all text-white font-bold"
-            href="#Contact"
+            className=" select-none cursor-pointer transition-all  font-bold"
+            href="#About"
           >
             Contact
-          </a>
-          <a
-            href="#"
-            className="px-4 py-2 border-2 border-white font-bold text-white rounded-2xl hover:text-black hover:bg-white"
+          </Link>
+          <Link
+            to="/about"
+            onClick={togglerDrawer}
+            className=" select-none cursor-pointer transition-all  font-bold"
+            href="#About"
           >
             Curriculum
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
