571c4851c3640a86c58824fa68456bd620440d19
Louisit0 <lrangarita7@gmail.com>
2023-07-11 14:06:21 -0300

    nueva estructura

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import Nav from "./Nav";
-import HomeCover from "./HomeCover";
-import About from "./About";
+import HomeCover from "../pages/HomeCover";
+import About from "../pages/About";
 import Skills from "./Skills";
-import Projects from "./Projects";
-import Contact from "./Contact";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
+import { Route, Switch } from "wouter";
 
 export const Layout = () => {
   return (
     // <UserContext.Provider value={user}>
-    <div className="min-h-screen">
-      <div className="flex flex-col">
-        <header>
-          <Nav />
-        </header>
-        <main className="flex flex-col w-full pb-4 max-w-4xl mx-auto px-8 sm:px-0">
-          <HomeCover />
-          <About />
-          <Skills />
-          <Projects />
-        </main>
-        <footer className="px-8 sm:px-0 max-w-4xl mx-auto w-full">
-          <Contact />
-        </footer>
-      </div>
+    <div className="flex flex-col">
+      <header>
+        <Nav />
+      </header>
+      <main className="flex flex-col w-full max-w-4xl mx-auto px-8 sm:px-0">
+        <Switch>
+          <Route path="/" component={HomeCover} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
+      </main>
+      <footer className="px-8 sm:px-0 max-w-4xl mx-auto w-full">
+        {/* <Contact /> */}
+      </footer>
     </div>
     /* </UserContext.Provider> */
   );
