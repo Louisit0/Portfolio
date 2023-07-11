ffa75a343b7f2254ab14093597c23cbe8c72ecbb
Louisit0 <lrangarita7@gmail.com>
2023-07-11 17:51:28 -0300

    nuevo about minimalista

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,34 +1,23 @@
-import avatar from "../assets/img/avatar.jpeg";
+import avatarl from "../assets/img/avatarl.jpg";
 
 const About = () => {
   return (
-    <section id="About" className="flex flex-col mx-auto mb-36 ">
-      <div className="flex flex-row w-1/2 mb-10 sm:mb-0 ">
-        <h2 className="text-2xl  font-bold">About me</h2>
-      </div>
-      <div className="md:flex md:flex-row flex flex-col-reverse">
-        <div className="md:w-3/5 md:order-1 md:text-lg flex flex-col justify-center">
-          <p className="md:text-lg my-auto ">
-            I am a junior frontend developer with knowledge of HTML, CSS,
-            JavaScript, React, and Razor C#. I excel at working in teams and
-            helping out whenever possible. I value feedback and strive for
-            personal growth. I am self-taught and currently studying to become a
-            systems analyst.
-          </p>
-        </div>
-        <div className="md:w-2/5 flex md:order-2">
-          <img
-            src={avatar}
-            alt="avatar"
-            className="opacity-70 hover:opacity-100 transition-opacity duration-300 rounded-xl w-32 h-32 md:w-3/5 md:h-full object-cover m-auto mb-10 sm-mb-0"
-          />
-        </div>
-      </div>
-      <div className="grid grid-cols-2 md:flex md:flex-wrap md:justify-between md:gap-6 gap-2 my-6 md:w-3/5 ">
-        <p>👨‍💻 Programar</p>
-        <p>🧠 Aprender</p>
-        <p>🎵 Música</p>
-        <p>🥊 Kickboxing</p>
+    <section
+      id="About"
+      className="flex flex-col justify-center text-base-content text-center mx-auto h-screen-luis "
+    >
+      <div className="flex flex-col items-center lg:flex-row lg:justify-around">
+        <img
+          src={avatarl}
+          alt="perfil"
+          className="h-64 w-64 md:h-72 md:w-72 lg:h-80 lg:w-80 rounded-full"
+        />
+        <p className="mt-8 lg:my-auto text-start md:text-center md:w-1/2">
+          A self-taught Software Engineer with experience working back-end for
+          the automotive and manufacturing industries. Recently, I started my
+          journey learning front-end development & discovered a fascination for
+          CSS Art.
+        </p>
       </div>
     </section>
   );
