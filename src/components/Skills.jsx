da207f3d9a480b3ea90ad0a4c00f2149e2f46148
Louisit0 <lrangarita7@gmail.com>
2023-07-13 17:23:23 -0300

    Iconos con colores y centrados

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,40 +12,40 @@ import {
 const Skills = () => {
   return (
     <div className="flex flex-col justify-center text-base-content text-center mx-8 md:mx-auto h-screen-custom">
-      <h3 className="text-start text-5xl font-extrabold mb-8">
+      <h3 className="text-start text-4xl md:text-5xl font-extrabold mb-8">
         My tech stack.
       </h3>
       <ul className="grid grid-cols-2 xl:grid-cols-4 gap-4">
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
-          <SiHtml5 size="1.3rem" className="self-center" />
-          <p className=" text-base ml-2"> Html</p>
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2  justify-center">
+          <SiHtml5 size="1.3rem" className="self-center text-red-500" />
+          <p className=" text-base ml-2"> HTML5</p>
         </li>
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
-          <SiCss3 size="1.3rem" className="self-center" />
-          <p className=" text-base ml-2"> Css</p>
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2 justify-center">
+          <SiCss3 size="1.3rem" className="self-center text-blue-500" />
+          <p className=" text-base ml-2"> CSS3</p>
         </li>
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
-          <SiJavascript size="1.3rem" className="self-center" />
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2  justify-center">
+          <SiJavascript size="1.3rem" className="self-center text-yellow-400" />
           <p className=" text-base ml-2"> Javascript</p>
         </li>
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
-          <SiReact size="1.3rem" className="self-center" />
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2  justify-center">
+          <SiReact size="1.3rem" className="self-center text-cyan-300" />
           <p className=" text-base ml-2"> React</p>
         </li>
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
-          <SiTailwindcss size="1.3rem" className="self-center" />
-          <p className=" text-base ml-2"> TailwindCss</p>
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2 justify-center">
+          <SiTailwindcss size="1.3rem" className="self-center text-cyan-400" />
+          <p className=" text-base ml-2"> Tailwind</p>
         </li>
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
-          <SiBootstrap size="1.3rem" className="self-center" />
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2 justify-center">
+          <SiBootstrap size="1.3rem" className="self-center text-purple-600" />
           <p className=" text-base ml-2"> Bootstrap</p>
         </li>
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2 justify-center">
           <SiGithub size="1.3rem" className="self-center" />
           <p className=" text-base ml-2"> Github</p>
         </li>
-        <li className="flex flex-row border-2 border-black rounded-2xl p-2">
-          <SiPostman size="1.3rem" className="self-center" />
+        <li className="flex flex-row border-2 border-black rounded-2xl p-2 justify-center">
+          <SiPostman size="1.3rem" className="self-center text-orange-500" />
           <p className=" text-base ml-2"> Postman</p>
         </li>
       </ul>
