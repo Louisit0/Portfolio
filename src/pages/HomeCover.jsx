e96f6caf6d176d3cee137c37836cd993a48ab449
Louisit0 <lrangarita7@gmail.com>
2023-07-12 23:53:26 -0300

    home con maquina de escribir y margenes arreglados

--- src/pages/HomeCover.jsx
+++ src/pages/HomeCover.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import gifGato from "../assets/img/gifGato.gif";
-import Index from "./Index";
-import Nav from "../components/Nav";
 
 const HomeCover = () => {
   return (
@@ -11,14 +9,16 @@ const HomeCover = () => {
         id="Home"
         className="flex flex-col justify-center text-base-content text-center mx-auto"
       >
-        <img src={gifGato} alt="pika" className="h-32 w-42 mx-auto my-4" />
-        <h1 className="text-4xl font-bold md:text-5xl mb-3">Hi, I'm Luis.</h1>
-        <h3 className="text-base md:text-lg md:w-3/5 mx-auto">
+        <img src={gifGato} alt="gato" className="h-32 w-42 mx-auto my-4" />
+        <div className="flex flex-row">
+          <span className="title-home text-4xl md:text-8xl">Hi, i'm Luis.</span>
+        </div>
+        <h3 className="text-base md:text-lg md:w-3/5 mx-auto my-4">
           Self-taught Frontend Developer based in Buenos Aires, Argentina 📍.
         </h3>
-        <div className="flex flex-row my-10 mx-auto">
-          <BsLinkedin size="1.5rem" className="mr-4 hover:fill-warning" />
-          <BsGithub size="1.5rem" className="hover:fill-warning" />
+        <div className="flex flex-row my-4 mx-auto">
+          <BsLinkedin size="1.5rem" className="mr-4" />
+          <BsGithub size="1.5rem" />
         </div>
       </section>
     </div>
