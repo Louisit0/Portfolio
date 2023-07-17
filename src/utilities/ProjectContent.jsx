aaef671ab89eabf7da27b618366a8e7ec4e5e349
Louisit0 <lrangarita7@gmail.com>
2023-07-17 17:38:02 -0300

    array de objetos de contenido de proyectos.

---
+++ src/utilities/ProjectContent.jsx
@@ -0,0 +1,29 @@
+import turisme from "../assets/img/turisme.jpg";
+import pokedex from "../assets/img/pokedex.png";
+
+const projectsContent = [
+  {
+    id: 1,
+    name: "turisme",
+    description:
+      "Es una tienda en línea donde puedes comprar paquetes de viajes, posee sección de paquetes y reseñas, un formulario de contacto y un carrito de compras.",
+    summary: "E-commerce para comprar paquetes turisticos.",
+    image: turisme,
+    technologies: ["JavaScript", "TailwindCSS", "SweetAlert2"],
+    github: "https://github.com/Louisit0/Tourism-web",
+    demo: "https://louisit0.github.io/Tourism-web/",
+  },
+  {
+    id: 2,
+    name: "Pokedéx",
+    description:
+      "Es una app donde puedes buscar y encontrar pokemones, ver sus caracteristicas, evoluciones, entre otras cosas.",
+    summary: "Buscador de pokemones desde la PokeApi.",
+    image: pokedex,
+    technologies: ["React", "TailwindCSS", "SweetAlert2"],
+    github: "https://github.com/Louisit0/Tourism-web",
+    demo: "https://louisit0.github.io/Tourism-web/",
+  },
+];
+
+export default projectsContent;
