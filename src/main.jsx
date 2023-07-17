b6e99f1ad7b7f2187f02ca9352c7914719c7ae70
Louisit0 <lrangarita7@gmail.com>
2023-07-17 01:20:12 -0300

    BroserRouter UPDATE

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./pages/About";
-import Skills from "./components/Skills";
-import Contact from "./pages/Contact";
-import HomeCover from "./pages/HomeCover";
-import Projects from "./pages/Projects";
-import Root from "./routes/root";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/techStack",
-        element: <Skills />,
-      },
-      {
-        index: true,
-        element: <HomeCover />,
-      },
-      {
-        path: "/projects",
-        element: <Projects />,
-      },
-    ],
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
+import App from "./routes/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
