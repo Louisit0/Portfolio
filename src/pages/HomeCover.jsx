db4bdc8c030a56decf49455f868deb7cc7857262
Louisit0 <lrangarita7@gmail.com>
2023-07-20 17:37:00 -0300

    nuevo diseño home

--- src/pages/HomeCover.jsx
+++ src/pages/HomeCover.jsx
@@ -1,23 +1,31 @@
-import { BsLinkedin, BsGithub } from "react-icons/bs";
+import {
+  BsLinkedin,
+  BsGithub,
+  BsFillEnvelopeFill,
+  BsTwitter,
+} from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const HomeCover = () => {
   return (
     <section
       id="Home"
-      className="flex flex-col justify-center items-center text-center mx-6 md:mx-8 lg:mx-0"
+      className="flex flex-col justify-center mx-6 md:mx-8 lg:mx-0"
     >
       {/* <img src={gifGato} alt="gato" className="h-32 w-42 mx-auto my-4" /> */}
-      <div className="flex flex-row justify-center">
-        <span className="title-home font-bold md:font-normal text-4xl md:text-7xl lg:text-8xl">
-          Hi, i'm Luis.
-        </span>
+      <div className="flex flex-row">
+        <h1 className="font-bold self-start text-start text-6xl md:text-9xl md:-ml-2">
+          LUIS RAMIREZ
+        </h1>
       </div>
-      <h3 className="text-sm md:text-base lg:text-lg mx-8 md:w-3/5 sm:mx-auto my-4 ">
-        Self-taught Frontend Developer based in Buenos Aires, Argentina 📍.
+      <h3 className="text-base self-start text-start lg:text-lg md:w-3/5 my-4 ">
+        Self-taught <span className="font-bold">Frontend Developer</span> based
+        in Buenos Aires, Argentina 📍.
       </h3>
-      <div className="flex flex-row my-4 mx-auto">
-        <BsLinkedin size="1.5rem" className="mr-4" />
+      <div className="flex flex-row mt-4 mb-6 w-1/2 md:w-1/3 justify-between">
+        <BsFillEnvelopeFill size="1.5rem" />
+        <BsTwitter size="1.5rem" />
+        <BsLinkedin size="1.5rem" />
         <BsGithub size="1.5rem" />
       </div>
       <div className="w-full mt-2 pb-4">
@@ -25,7 +33,7 @@ const HomeCover = () => {
           to={"/about"}
           className=" select-none cursor-pointer transition-all font-bold"
         >
-          <div className="flex flex-row w-full justify-start px-6 md:px-0">
+          <div className="flex flex-row w-full justify-start md:px-0">
             <p className="mr-4 text-sm self-center">About me</p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
